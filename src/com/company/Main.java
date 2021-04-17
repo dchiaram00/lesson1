486301f33da6ae61abb9c01c258836117c684dd9
Dave Chiaramonte <45909443+dchiaram00@users.noreply.github.com>
2021-04-17 15:05:01 -0500

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Define variables to illustrate data types
+        int intValue1;
+        int intValue2;
+        int intValue3;
+        double dbValue1;
+        double dbValue2;
+        double dbValue3;
+
+    // Set the variables to a starting value
+        intValue1 = 25;
+        intValue2 = 50;
+
+        dbValue1 = 25.032453;
+        dbValue2 = 50.2341534;
+
+    // Do some math
+        intValue3 = intValue1 + intValue2;
+        System.out.println("Value1 = " + intValue1 );
+        System.out.println("Value2 = " + intValue2 );
+        System.out.println("Value1 + Value2 = " + intValue3);
+
+     // Do some math
+        dbValue3 = dbValue1 + dbValue2;
+        System.out.println("Value1 = " + dbValue1 );
+        System.out.println("Value2 = " + dbValue2 );
+        System.out.println("Value1 + Value2 = " + dbValue3);
+
+
+
+    }
+}
